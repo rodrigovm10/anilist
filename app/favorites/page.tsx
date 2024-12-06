ec57664ad3bddb54a1f3524fa9cbcef85f5dea4b
rodrigovm0420 <rvega@becompliance.es>
2024-12-05 23:12:53 -0600

    🔨 Fix cache errors

--- app/favorites/page.tsx
+++ app/favorites/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { AnimeCard } from '@/components/anime-card'
+import { useGetAnimesByUserId } from '@/db/queries'
 import { useAuth } from '@/hooks/useAuth'
 import { useLocalStorage } from '@/hooks/useLocalStorage'
 import { Anime } from '@/types/anime'
@@ -8,8 +9,8 @@ import { useRouter } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function FavoritesPage() {
-  const [animesFavorites] = useLocalStorage<Anime[]>('animes-favorites', [])
-  const { isAuthenticated } = useAuth()
+  const { isAuthenticated, user } = useAuth()
+  const animesFavorites = useGetAnimesByUserId(user.id)
   const router = useRouter()
 
   useEffect(() => {
@@ -22,16 +23,17 @@ export default function FavoritesPage() {
     <main className='container mx-auto px-4 py-8'>
       <h1 className='font-bold text-3xl mb-8'>Your Favorites</h1>
       <section className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6'>
-        {animesFavorites.length > 0 ? (
-          animesFavorites.map(anime => (
+        {animesFavorites?.length === 0 && (
+          <p className='text-gray-500'>You have no favorite animes yet.</p>
+        )}
+        {animesFavorites &&
+          animesFavorites?.length > 0 &&
+          animesFavorites?.map(anime => (
             <AnimeCard
               key={anime._id}
               anime={anime}
             />
-          ))
-        ) : (
-          <p className='text-gray-500'>You have no favorite animes yet.</p>
-        )}
+          ))}
       </section>
     </main>
   )
